c058c0eca036d1897f9caa9f0e1751f821978aba
kelincicodes <kelincicodes@gmail.com>
2022-07-23 23:12:38 +0800

    Added gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.7",
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+    only: [],
+  },
   paths: { tests: "tests" },
 };
 
